8ae44199e0c257b43141e585b468eff0b67a5c20
Valaubr <Stalker039906@gmail.com>
2020-03-19 20:51:52 +0300

    Bean scope

--- src/main/java/ru/valaubr/learn/testSpring.java
+++ src/main/java/ru/valaubr/learn/testSpring.java
@@ -18,10 +18,11 @@ public class testSpring {
 
 //        Music music = context.getBean("musicBean", Music.class);
         MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer mp2 = context.getBean("musicPlayer", MusicPlayer.class);
         mp.playMusic();
         
-        System.out.println(mp.getName());
-        System.out.println(mp.getVolume());
+        System.out.println(mp == mp2);
+        //System.out.println(mp.getVolume());
         context.close();
     }
 }
