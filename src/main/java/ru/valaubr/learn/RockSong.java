c11306eee53ee0e4a9c3f422ec5677dc8b8efdc7
Valaubr <Stalker039906@gmail.com>
2020-03-21 11:23:14 +0300

    XML Finish.

--- src/main/java/ru/valaubr/learn/RockSong.java
+++ src/main/java/ru/valaubr/learn/RockSong.java
@@ -5,12 +5,29 @@
  */
 package ru.valaubr.learn;
 
+import java.util.Date;
+
 /**
  *
  * @author valaubr
  */
 public class RockSong implements Music{
-
+    
+    private RockSong(){}
+    
+    public static RockSong getRockSong(){
+        return new RockSong();
+    }
+    
+    public void initMethod(){
+        System.out.println("You have a good taste");
+    }
+    
+    public void destroyMethod(){
+        Date date = new Date();
+        System.out.println("i`m die at: " + date.toString());
+    }
+        
     @Override
     public String getSong() {
         return "Starset - Perfect Machine";
--- src/main/java/ru/valaubr/learn/testSpring.java
+++ src/main/java/ru/valaubr/learn/testSpring.java
@@ -16,13 +16,16 @@ public class testSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-//        Music music = context.getBean("musicBean", Music.class);
+        Music rockMusic = context.getBean("musicBean", RockSong.class);
+        System.out.println(rockMusic.getSong());
+        context.close();
+/*        Music music = context.getBean("musicBean", Music.class);
         MusicPlayer mp = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer mp2 = context.getBean("musicPlayer", MusicPlayer.class);
         mp.playMusic();
         
         System.out.println(mp == mp2);
         //System.out.println(mp.getVolume());
-        context.close();
+        context.close();*/
     }
 }
